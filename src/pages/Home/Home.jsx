2b873726e54804c436d0bbac7ca62241d56d4cee
hsj031232 <hsj031232@gmail.com>
2026-04-14 00:00:13 +0900

    feat: 홈 배너에 앨범 생성 버튼 추가 및 /Creates1 연결

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-﻿import styled from 'styled-components';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import HomeBanner from '../../assets/images/HomeBanner.png';
 import Travelog from '../../components/Travelog/Travelog';
@@ -39,6 +40,26 @@ const TextContainer = styled.div`
   gap: 2vw;
   align-items: center;
 `;
+
+const AlbumCreateButton = styled(Link)`
+  margin-top: 1vw;
+  padding: 1vw 2.8vw;
+  border-radius: 0.2vw;
+  background: #5e81ff;
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 1vw;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  text-decoration: none;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.92;
+  }
+`;
+
 const Home = () => {
   return (
     <>
@@ -48,6 +69,7 @@ const Home = () => {
         <TextContainer>
           <BannerText2>당신의 순간으로부터,</BannerText2>
           <BannerText2>맞춤형 여행 앨범으로 추억을 되새겨 보세요!</BannerText2>
+          <AlbumCreateButton to="/Creates1">앨범 생성</AlbumCreateButton>
         </TextContainer>
       </StyledBannerContainer>
 
