4f962f7dcc0e2c3ad8a526d9a208a5d4270b2995
Stargaze <molmul99@gmail.com>
2026-05-05 16:43:33 +0900

    feat: Google/Kakao OAuth 로그인 기능 구현

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,9 @@
 import Header from '../../components/Header/Header';
 import styled from 'styled-components';
-import kakao from '../../assets/images/kakao.png';
+import kakaoLoginBtn from '../../assets/images/kakao_login_btn.png';
+import googleLoginBtn from '../../assets/images/google_login_btn.png';
 import { useState } from 'react';
-import axios from 'axios';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Container = styled.div`
   display: flex;
@@ -14,7 +14,6 @@ const LoginContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 60%;
-  /* background-color: yellow; */
   margin-top: 4vw;
   height: 40vw;
   align-items: center;
@@ -109,12 +108,6 @@ const SimpleStroke = styled.div`
   stroke: var(--2, #902a2a);
 `;
 
-const KakaoContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 2vw;
-  margin-top: 1.2vw;
-`;
 const SimpleText = styled.p`
   color: #000;
   font-family: Pretendard;
@@ -123,10 +116,25 @@ const SimpleText = styled.p`
   font-weight: 500;
   line-height: normal;
 `;
+
+const SocialContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1vw;
+  margin-top: 1.2vw;
+  align-items: center;
+`;
+
+const SocialLoginBtn = styled.button`
+  background: none;
+  border: none;
+  padding: 0;
+  cursor: pointer;
+`;
+
 const Login = () => {
   const [loginId, setLoginId] = useState('');
   const [password, setPassword] = useState('');
-
   const navigate = useNavigate();
 
   const handleSignup = () => {
@@ -134,46 +142,41 @@ const Login = () => {
   };
 
   const handleLogin = async () => {
-    const requestBody = {
-      loginId,
-      password,
-    };
-
+    const requestBody = { loginId, password };
     try {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(requestBody),
       });
 
-      if (!response.ok) {
-        throw new Error(`Login failed with status: ${response.status}`);
-      }
+      if (!response.ok) throw new Error(`Login failed with status: ${response.status}`);
 
       const contentType = response.headers.get('Content-Type');
-      if (!contentType || !contentType.includes('application/json')) {
+      if (!contentType || !contentType.includes('application/json'))
         throw new Error('Invalid content type received from server');
-      }
 
       const data = await response.json();
       console.log('Login successful:', data);
-      navigate('/');
 
       const authToken = response.headers.get('authorization');
       const refreshToken = response.headers.get('authorization-refresh');
-
-      console.log('Auth Token:', authToken);
-      console.log('Refresh Token:', refreshToken);
-
       localStorage.setItem('authToken', authToken);
       localStorage.setItem('refreshToken', refreshToken);
-
       localStorage.setItem('userId', data.result.user_id);
+
+      navigate('/');
     } catch (error) {
       console.error('Error during login:', error.message);
     }
+  };
+
+  const handleKakaoLogin = () => {
+    window.location.href = `${process.env.REACT_APP_API_URL}/oauth2/authorization/kakao`;
+  };
+
+  const handleGoogleLogin = () => {
+    window.location.href = `${process.env.REACT_APP_API_URL}/oauth2/authorization/google`;
   };
 
   return (
@@ -187,12 +190,12 @@ const Login = () => {
               placeholder="아이디를 입력해주세요"
               value={loginId}
               onChange={(e) => setLoginId(e.target.value)}
-            ></InputField>
+            />
             <InputField
               placeholder="비밀번호를 입력해주세요"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-            ></InputField>
+            />
           </InputContainer>
           <FindText>아이디 찾기 | 비밀번호 찾기</FindText>
           <LoginBtn onClick={handleLogin}>로그인하기</LoginBtn>
@@ -202,10 +205,14 @@ const Login = () => {
             <SimpleText>간편 로그인</SimpleText>
             <SimpleStroke />
           </Simplecontainer>
-          <KakaoContainer>
-            <img src={kakao} style={{ width: '2.6vw', height: '2.6vw' }} />
-            <img src={kakao} style={{ width: '2.6vw', height: '2.6vw' }} />
-          </KakaoContainer>
+          <SocialContainer>
+            <SocialLoginBtn onClick={handleKakaoLogin}>
+              <img src={kakaoLoginBtn} style={{ width: '13vw', height: 'auto' }} />
+            </SocialLoginBtn>
+            <SocialLoginBtn onClick={handleGoogleLogin}>
+              <img src={googleLoginBtn} style={{ width: '13vw', height: 'auto' }} />
+            </SocialLoginBtn>
+          </SocialContainer>
         </LoginContainer>
       </Container>
     </>
